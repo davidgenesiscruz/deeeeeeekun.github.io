bf468b67033953cc7b63a8fa6bca1a149881429f
David Genesis Geniza Cruz <david.genesis.cruz@bizreach.co.jp>
2015-07-15 13:56:27 +0900

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -22,7 +22,7 @@ function shuffler(shuffleID) {
   var curImage = imageCounter[shuffleID][1];
   var maxImage = imageCounter[shuffleID][2];
   var top = document.getElementById("shuffle" + shuffleID + curImage);
-  hide(top);
+  top.classList.remove('visible');
 
   if (curImage === maxImage) {
     curImage = 0;
@@ -32,16 +32,5 @@ function shuffler(shuffleID) {
 
   var next = document.getElementById("shuffle" + shuffleID + curImage);
   imageCounter[shuffleID][1] = curImage;
-  show(next);
-}
-
-function show(element) {
-  var classes = element.className;
-  classes += " visible";
-  element.className = classes;
-}
-
-function hide(element) {
-  var classes = element.className;
-  element.className = classes.replace(" visible", "");
+  next.classList.add('visible');
 }
